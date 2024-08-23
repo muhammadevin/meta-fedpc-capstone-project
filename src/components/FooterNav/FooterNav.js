466d91834416bafd3745a6b4439bf50d769262f2
Muhammad Pratista <muhammad.pratista@gdn-commerce.com>
2024-08-24 00:51:31 +0700

    Modify footer navigation & add package

--- src/components/FooterNav/FooterNav.js
+++ src/components/FooterNav/FooterNav.js
@@ -1,12 +1,14 @@
+import './footernav.css'
+
 const FooterNav = (props) => {
   return (
     <div className='footer-navigation'>
       <p className="section-categories">{props.navCategory}</p>
       <nav>
-        <ul>
+        <ul className="footer-navigation__section">
           {props.navItems.map((nav, index) => (
             <li key={index}>
-              <a href={`#${nav}`}>
+              <a href={`#${nav}`} className="footer-navigation__items">
                 <p>{nav}</p>
               </a>
             </li>

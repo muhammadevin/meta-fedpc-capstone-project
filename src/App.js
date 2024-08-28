10651969065aa6e8a92f827b6a596e6de4e21084
Muhammad Pratista <muhammad.pratista@gdn-commerce.com>
2024-08-28 12:44:36 +0700

    Add routing configuration and refactor page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import Footer from './components/Footer/Footer';
 import Sidebar from './components/Sidebar/Sidebar';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import BookingPage from './components/BookingPage/BookingPage';
 
 function App() {
   const [sidebarOpen, setSideBarOpen] = useState(false);
@@ -15,7 +17,10 @@ function App() {
     <>
       <Sidebar isOpen={sidebarOpen} toggleSidebar={handleViewSidebar} />
       <Header toggleSidebar={handleViewSidebar}/>
-      <Main />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/reservation' element={<BookingPage />}/>
+      </Routes>
       <Footer />
     </>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
+
+function Home() {
+  return (
+    <>
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
